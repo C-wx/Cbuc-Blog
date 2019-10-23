d25dcdb58caab2c1e18ff0837b9dcf3ebff8b50b
Cbuc <1404668149@qq.com>
2019-10-23 17:12:24 +0800

    init commit

---
+++ src/main/java/cbuc/blog/CbucApplication.java
@@ -0,0 +1,13 @@
+package cbuc.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbucApplication.class, args);
+    }
+
+}
