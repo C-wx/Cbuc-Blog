4e7836541e55f2eb2307321f92c75363e3fdee32
Cbuc <1404668149@qq.com>
2019-11-13 09:14:41 +0800

    工具类更改 commit

--- src/main/java/cbuc/blog/utils/IPutil.java
+++ src/main/java/cbuc/blog/utils/IPutil.java
@@ -1,7 +1,7 @@
 package cbuc.blog.utils;
 
 
-import org.apache.commons.lang.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
  * @Version 1.0
  * @Date 2019/10/30
  */
+@Slf4j
 public class IPutil {
 
     /**
@@ -18,53 +19,62 @@ public class IPutil {
      * @param  request
      * @Return  ip地址
      */
-    public static String getIpAddress(HttpServletRequest request)
-    {
-        String ip = null;
-        try
-        {
-            // 获取用户真是的地址
-            String Xip = request.getHeader("X-Real-IP");
-            // 获取多次代理后的IP字符串值
-            String XFor = request.getHeader("X-Forwarded-For");
-            if (StringUtils.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor))
-            {
-                // 多次反向代理后会有多个IP值，第一个用户真实的IP地址
-                int index = XFor.indexOf(",");
-                if (index >= 0)
-                {
-                    return XFor.substring(0, index);
-                } else
-                {
-                    return XFor;
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (log.isInfoEnabled()) {
+            log.info("getIpAddress(HttpServletRequest) - X-Forwarded-For - String ip=" + ip);
+        }
+
+        if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+            if (ip.length() > 15) {
+                String[] ips = ip.split(",");
+
+                for(int index = 0; index < ips.length; ++index) {
+                    String strIp = ips[index];
+                    if (!"unknown".equalsIgnoreCase(strIp)) {
+                        ip = strIp;
+                        break;
+                    }
                 }
             }
-            ip = Xip;
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip))
-            {
+        } else {
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
                 ip = request.getHeader("Proxy-Client-IP");
+                if (log.isInfoEnabled()) {
+                    log.info("getIpAddress(HttpServletRequest) - Proxy-Client-IP - String ip=" + ip);
+                }
             }
-            if (StringUtils.isEmpty(ip) || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
-            {
+
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
                 ip = request.getHeader("WL-Proxy-Client-IP");
+                if (log.isInfoEnabled()) {
+                    log.info("getIpAddress(HttpServletRequest) - WL-Proxy-Client-IP - String ip=" + ip);
+                }
             }
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip))
-            {
+
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
                 ip = request.getHeader("HTTP_CLIENT_IP");
+                if (log.isInfoEnabled()) {
+                    log.info("getIpAddress(HttpServletRequest) - HTTP_CLIENT_IP - String ip=" + ip);
+                }
             }
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip))
-            {
+
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
                 ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+                if (log.isInfoEnabled()) {
+                    log.info("getIpAddress(HttpServletRequest) - HTTP_X_FORWARDED_FOR - String ip=" + ip);
+                }
             }
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip))
-            {
+
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
                 ip = request.getRemoteAddr();
+                if (log.isInfoEnabled()) {
+                    log.info("getIpAddress(HttpServletRequest) - getRemoteAddr - String ip=" + ip);
+                }
             }
-        } catch (Exception e)
-        {
-            e.printStackTrace();
         }
-        return ip;
+
+        return ip.equals("0:0:0:0:0:0:0:1") ? "127.0.0.1" : ip;
     }
 
 }
